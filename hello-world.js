d20ef35e08dcc2ddb6dc3e76326a88d62863b110
yilai <yi_lai@outlook.com>
2017-01-16 18:56:45 +0000

    printed wrong info for #1, so re-typed the answer with correct answer.

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,8 @@
+//Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
+
 console.log('start of program');
 setTimeout(function(){
-	console.log("end of program");
+	console.log("Hello World Again!");
 },10000);
-console.log("this is Really the end of program");
+
+//If you never tried setInterval, give it a try first. It works the same way as setTimeout, takes a callback function and a time in milliseconds. But instead of calling your callback once, it calls it once every x milliseconds. In this exercise, you'll have to mimic what setInterval is doing but only with setTimeout!
