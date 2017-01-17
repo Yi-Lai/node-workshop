f49ecba24d8b848a6bf98d57942506d9b64135d0
yilai <yi_lai@outlook.com>
2017-01-17 19:58:54 +0000

    This is the number guessing game

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -1,18 +1,37 @@
 // In this file, re-write your number guessing game (from the basic javascript workshop) for the command line!
 // Instead of using prompt and alert, you will have to use capabilities from NodeJS and any external module. 
 //Generate a random number between 1 and 100. Ask the user to guess the number. You should give them 4 tries to guess the number. If they guess wrong, tell them if it's higher or lower. If they guess right, congratulate them. Otherwise, give them a message saying what the correct number was, as well as their list of guesses.
-
+var target = Math.floor(Math.random() * 100);
 var prompt = require("prompt");
+var c;
 
-prompt.get(["Choose a random number"]),function(answer,result){
-	if(answer === 5){
-		console.log('something bad');
-	}else{3
-
-		//log the result
-
-		console.log('Command-line input received')
-		console.log(" username: " + result.username)
-		console.log(" email: " + result.email)
+function game(c) { 
+	if (c === 0) {
+		console.log("game over!")
 	}
-});
+	else {
+		prompt.get(["Choose a random number"], function(err, res) {
+			var actualnumber = parseInt(res['Choose a random number']);
+			if (typeof actualnumber !== "number") {
+				console.log("This is not a number");
+			}
+			else {
+
+				if (actualnumber == target) {
+					console.log("win!")
+				}
+				else {
+					if (actualnumber > target) {
+						console.log("high")
+					}
+					else {
+						console.log("low")
+					}
+				game(c-1);
+				}
+			}
+		});
+	}
+}
+
+game(4);
